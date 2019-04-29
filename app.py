5faf23184e2c2e7b5b136347ad51e0f7cf38162f
José Domingo <josedom24@gmail.com>
2019-04-29 12:28:54 +0200

    versión 2

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template,request
+import os
+from flask import Flask, render_template,request,abort
 app = Flask(__name__)	
 
 
 @app.route('/',methods=["GET","POST"])
 def inicio():
 	return render_template("inicio.html")
+
+@app.route('/temperatura',methods=["POST"])
+def temperatura():
+    key=os.environ["key"]
+    ciudad=request.form.get("ciudad")
+    payload = {'q': ciudad, 'mode': 'xml','units':'metric','APPID':key}
+    r=requests.get('http://api.openweathermap.org/data/2.5/weather',params=payload)
+    if r.status_code == 200:
+	    doc = etree.fromstring(r.text.encode ('utf-8'))
+	    temp=doc.xpath("temperature/@value")[0]
+	    return render_template("temperatura.html",ciudad=ciudad,temperatura=temp)
 
 port=os.environ["PORT"]
 app.run('0.0.0.0',int(port), debug=True)
